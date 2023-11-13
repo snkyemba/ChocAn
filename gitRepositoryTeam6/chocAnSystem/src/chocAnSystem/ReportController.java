1b3bbfe7c17c15d6db278888c21aee77e0dc8168
snkyemba <snkyemba@crimson.ua.edu>
2023-11-13 16:17:27 -0600

    Updated terminal and controller

--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ReportController.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ReportController.java
@@ -6,6 +6,37 @@ import chocAnSystem.ManagerReport;
 
 public class ReportController {
 	
-	private Report model;
+	private MemberReport memberReport;
+	private ManagerReport managerReport;
+	private ProviderReport providerReport;
 	private ManagerTerminal view;
+	
+	public ReportController() {
+        this.memberReport = new MemberReport("John Doe", 123, "123 Main St", "City", "State", 12345);
+        this.managerReport = new ManagerReport();
+        this.providerReport = new ProviderReport("Provider Name", 456, "456 Oak St", "City", "State", 67890);
+    }
+
+    public void generateMemberReport() {
+        // Additional logic to generate member report
+        // For example:
+        memberReport.addService(new MemberReport.Service("01-01-22", "Provider1", "Service1"));
+        //memberReport.toFile();
+    }
+
+    public void generateManagerReport() {
+        // Additional logic to generate manager report
+        // For example:
+        ManagerReport.Provider provider1 = new ManagerReport.Provider("Provider1", 10, 100);
+        managerReport.addProvider(provider1);
+        managerReport.calculateTotals();
+        //managerReport.toFile();
+    }
+
+    public void generateProviderReport() {
+        // Additional logic to generate provider report
+        // For example:
+        providerReport.addService(new ProviderReport.Service("01-01-22", "Service1", 50.0));
+        //providerReport.toFile();
+    }
 }
