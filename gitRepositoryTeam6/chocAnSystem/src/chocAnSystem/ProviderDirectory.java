6d3761236b19979c0329f67e93c52af95afb9087
wwmink <wwmink@crimson.ua.edu>
2023-11-13 16:05:41 -0600

    Started filling out basics for ProviderDirectory

--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ProviderDirectory.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ProviderDirectory.java
@@ -1,6 +1,19 @@
 package chocAnSystem;
 
-public class ProviderDirectory
+public class ProviderDirectory {
+    private int serviceCode;
+    private String serviceName;
 
-{
+    public ProviderDirectory(int serviceCode, String serviceName) {
+        this.serviceCode = serviceCode;
+        this.serviceName = serviceName;
+    }
+
+    public int getServiceCode() {
+        return serviceCode;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
 }
--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ServiceRecord.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ServiceRecord.java
@@ -2,44 +2,43 @@ package chocAnSystem;
 import java.util.Date;
 
 public class ServiceRecord {
+    private Date currentDate;
+    private Date serviceDate;
+    private int providerNumber;
+    private int memberNumber;
+    private int serviceCode;
+    private String comments;
 
-        private Date currentDate;
-        private Date serviceDate;
-        private int providerNumber;
-        private int memberNumber;
-        private int serviceCode;
-        private String comments;
+    public ServiceRecord(Date currentDate, Date serviceDate, int providerNumber, int memberNumber, int serviceCode, String comments) {
+        this.currentDate = currentDate;
+        this.serviceDate = serviceDate;
+        this.providerNumber = providerNumber;
+        this.memberNumber = memberNumber;
+        this.serviceCode = serviceCode;
+        this.comments = comments;
+    }
 
-        public ServiceRecord(Date currentDate, Date serviceDate, int providerNumber, int memberNumber, int serviceCode, String comments) {
-            this.currentDate = currentDate;
-            this.serviceDate = serviceDate;
-            this.providerNumber = providerNumber;
-            this.memberNumber = memberNumber;
-            this.serviceCode = serviceCode;
-            this.comments = comments;
-        }
+    public Date getCurrentDate() {
+        return currentDate;
+    }
 
-        public Date getCurrentDate() {
-            return currentDate;
-        }
+    public Date getServiceDate() {
+        return serviceDate;
+    }
 
-        public Date getServiceDate() {
-            return serviceDate;
-        }
+    public int getProviderNumber() {
+        return providerNumber;
+    }
 
-        public int getProviderNumber() {
-            return providerNumber;
-        }
+    public int getMemberNumber() {
+        return memberNumber;
+    }
 
-        public int getMemberNumber() {
-            return memberNumber;
-        }
+    public int getServiceCode() {
+        return serviceCode;
+    }
 
-        public int getServiceCode() {
-            return serviceCode;
-        }
-
-        public String getComments() {
-            return comments;
-        }
+    public String getComments() {
+        return comments;
+    }
 }
