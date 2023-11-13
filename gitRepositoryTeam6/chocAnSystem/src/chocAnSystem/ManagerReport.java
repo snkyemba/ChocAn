1ab9bfdd8be4cc7d5d76c609e2ee1a3f0fff420d
echilders2004 <epchilders@crimson.ua.edu>
2023-11-12 20:42:52 -0600

    Just committing my changes before I go to sleep

--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ManagerReport.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ManagerReport.java
@@ -1,13 +1,18 @@
 package chocAnSystem;
 import java.util.Vector;
 import java.util.Iterator;
-
+//Manager Report class by EvanChilders
 public class ManagerReport {
 	
 	class Provider{
-		String provider;
-		int numConsult;
-		int fee;
+		private String provider;
+		private int numConsult;
+		private int fee;
+		
+		public Provider(String provider, int numConsult, int fee) {
+			this.provider = provider;
+			this.numConsult = numConsult;
+			this.fee = fee;
 	}
 	Vector<Provider> providerList = new Vector<>();
 	int numProviders;
@@ -19,6 +24,8 @@ public class ManagerReport {
 		providerList.add(provider);
 		
 	}
+	//runs through the provider vector and adds up the total amount of consultations and fees
+	//potentially add something that clears the vector at the end of each week?
 	public void calculateTotals() {
 		Iterator<Provider> iterator = providerList.iterator();
 		numProviders = providerList.size();
@@ -35,4 +42,28 @@ public class ManagerReport {
 	public void fromFile() {
 		//figure out formatting for vector data from file or database
 	}
+	//getter and setter methods
+	public String getProvider() {
+		return provider;
+	}
+	public int getNumConsult() {
+		return numConsult;
+	}
+	public int getFee() {
+		return fee;
+	}
+	public Vector<Provider> getProviderList() {
+		return providerList;
+	}
+	public void setProvider(String newProvider) {
+		provider = newProvider;
+	}
+	public void setNumConsult(int newNumConsult) {
+		numConsult = newNumConsult;
+	}
+	public void setFee(int newFee) {
+		fee = newFee;
+	}
+	
+}
 }
--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/MemberReport.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/MemberReport.java
@@ -1,5 +1,79 @@
 package chocAnSystem;
+//import chocAnSystem.ManagerReport;
+import java.util.Vector;
 
 public class MemberReport {
+	private String name;
+	private int memNumber;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	
 
+	
+		class Service{
+			//note that I changed date to type String instead of Date type because it should
+			//be in the form "MM-DD-YY"
+			private String date;
+			private String providerName;
+			private String serviceName;
+			
+			public Service(String date, String providerName, String serviceName) {
+				this.date = date;
+				this.providerName = providerName;
+				this.serviceName = serviceName;
+				
+			}
+			public String getDate(){
+				return date;
+			}
+			public String getProviderName() {
+				return providerName;
+			}
+			public String getServiceName() {
+				return serviceName;
+			}
+	}
+		Vector<Service> serviceList;
+	
+	public MemberReport(String name, int memNumber, String address, String city, String state, int zip){
+		this.name = name;
+		this.memNumber = memNumber;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	public void addService(Service newService) {
+		serviceList.add(newService);
+	}
+	public void toFile() {
+		//figure out formatting for vector data into file or database
+	}
+	public void fromFile() {
+		//figure out formatting for vector data from file or database
+			}
+	public String getName() {
+		return name;
+	}
+	public int getNumber() {
+        return memNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
 }
