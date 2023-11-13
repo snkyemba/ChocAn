0b51a4a901a2e61927b7874e0cf77e67c0805872
snkyemba <snkyemba@crimson.ua.edu>
2023-11-12 19:18:29 -0600

    Working on the terminal and small edit to controller

--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ReportController.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ReportController.java
@@ -1,8 +1,11 @@
 package chocAnSystem;
+import chocAnSystem.Report;
 import chocAnSystem.ProviderReport;
 import chocAnSystem.MemberReport;
 import chocAnSystem.ManagerReport;
 
 public class ReportController {
-
+	
+	private Report model;
+	private ManagerTerminal view;
 }
