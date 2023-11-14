cdd890930b8eabd244dbce198cf0d5157888dc6e
echilders2004 <epchilders@crimson.ua.edu>
2023-11-14 14:01:27 -0600

    some quirky changes to the GUI

--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/TerminalGUI.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/TerminalGUI.java
@@ -3,21 +3,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import javax.sound.sampled.*;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 
 public class TerminalGUI extends JFrame {
-
+	
+	private Clip clip;
+	
     public TerminalGUI() {
         // Set up the main frame
         setTitle("Terminal Selector");
-        setSize(400, 200);
+        setSize(1200, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        initClip();
         // Create buttons
+        JLabel welcome = new JLabel("Welcome to the Chocoholics Software!", SwingConstants.CENTER);
         JButton managerButton = new JButton("Manager Terminal");
         JButton providerButton = new JButton("Provider Terminal");
         JButton operatorButton = new JButton("Operator Terminal");
-
+        ImageIcon emoji = new ImageIcon(getClass().getResource("/NoEmoji.jpeg"));
+        JLabel noEmoji = new JLabel(emoji);
+        ImageIcon chocolatePic = new ImageIcon(getClass().getResource("/Chocolate.jpeg"));
+        JLabel chocolate = new JLabel(chocolatePic);
+        
         // Add action listeners to the buttons
         managerButton.addActionListener(new ActionListener() {
             @Override
@@ -39,12 +56,56 @@ public class TerminalGUI extends JFrame {
                 openOperatorTerminal();
             }
         });
+        
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        welcome.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
+        mainPanel.add(welcome, BorderLayout.NORTH);
+        
+        JPanel imagePanel = new JPanel (new FlowLayout());
+        imagePanel.add(noEmoji);
+        imagePanel.add(chocolate);
+        mainPanel.add(imagePanel, BorderLayout.CENTER);
+        
+        JPanel buttonPanel = new JPanel(new GridLayout(1,3));
+        
+        Dimension buttonSize = new Dimension(600, 200);
+        	managerButton.setPreferredSize(buttonSize);
+        	providerButton.setPreferredSize(buttonSize);
+        	operatorButton.setPreferredSize(buttonSize);
+        	
+        buttonPanel.add(managerButton);
+        buttonPanel.add(providerButton);
+        buttonPanel.add(operatorButton);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        
+       setContentPane(mainPanel);
 
-        // Set up the layout
-        setLayout(new GridLayout(3, 1));
-        add(managerButton);
-        add(providerButton);
-        add(operatorButton);
+       playBackgroundMusic();
+    }
+    private void initClip() {
+        try {
+            // Load audio file from the same directory as the program
+            URL audioFileURL = getClass().getResource("/song.wav");
+
+            if (audioFileURL != null) {
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFileURL);
+                clip = AudioSystem.getClip();
+                clip.open(audioStream);
+            } else {
+                System.err.println("Audio file not found.");
+            }
+
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+            e.printStackTrace(); // Handle exceptions appropriately
+        }
+    }
+
+
+
+    private void playBackgroundMusic() {
+        if (clip != null) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
     }
 
     private void openManagerTerminal() {
