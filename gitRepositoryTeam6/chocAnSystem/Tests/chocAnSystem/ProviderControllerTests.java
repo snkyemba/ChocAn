d37f0ec9803d78c874fbbb200f6f13cf5a110102
wwmink <wwmink@crimson.ua.edu>
2023-11-28 13:43:57 -0600

    Working on getting file writing working

--- gitRepositoryTeam6/chocAnSystem/Tests/chocAnSystem/ProviderControllerTests.java
+++ gitRepositoryTeam6/chocAnSystem/Tests/chocAnSystem/ProviderControllerTests.java
@@ -1,7 +1,22 @@
 package chocAnSystem;
 
+import org.junit.Test;
+import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class ProviderControllerTests {
+    int serviceCode = 123456;
+    String serviceName = "Test Service";
+    float serviceFee = 123.45f;
+    String filePath = "chocAnSystem/TestFiles/testProviderDirectory.json";
 
+    @Test
+    public void testSaveServiceType() {
+        ProviderController testController = new ProviderController();
+        try {
+            testController.saveServiceType(serviceCode, serviceName, serviceFee, filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ProviderController.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ProviderController.java
@@ -27,30 +27,19 @@ public class ProviderController {
 
     /*
     Testing method for creating a provider directory with entries. Takes service info and a file path as parameters.
-    If the file or folder do not exist, they will be created. If they do exist, the new entries will be appended to
-    the end of the file... I hope.
+    If the file doesn't exist, it will be created. If it does exist, the new entry will be appended to the end of the file.
      */
-    public void saveServiceType(int code, String name, float fee, String filePath) {
-        // Create a new instance of the ProviderDirectory class
-        ProviderDirectory service = new ProviderDirectory(code, name, fee);
+    public void saveServiceType(int code, String name, float fee, String filePath) throws IOException {
+        ProviderDirectory entry = new ProviderDirectory(code, name, fee);
+        String jsonString = entry.toJson();
 
-        // Convert the ProviderDirectory instance to a JSON string
-        String jsonString = service.toJson();
-
-        // Create a new file object for the file we want to save to
-        File file = new File(filePath);
-
-        // Create the folders on the absolute path if they don't exist
-        // TODO: Does this even work?
-        file.getParentFile().mkdirs();
-
-        // Write the JSON string to the file
         try {
-            FileWriter writer = new FileWriter(file, true);
-            writer.write(jsonString);
+            FileWriter writer = new FileWriter(filePath, true);
+            writer.write(jsonString + "\n");
             writer.close();
         } catch (IOException e) {
-            //TODO: Handle this exception
+            e.printStackTrace();
+            throw new IOException("Error writing to file");
         }
     }
 }
