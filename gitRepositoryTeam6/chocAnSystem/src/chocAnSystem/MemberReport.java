e14c05b6c583bee5658d2a990386159cde64082b
wwmink <wwmink@crimson.ua.edu>
2023-11-28 12:34:22 -0600

    losing my mind

--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/MemberReport.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/MemberReport.java
@@ -1,13 +1,5 @@
 package chocAnSystem;
 
 public class MemberReport {
-	public void memberReport(String name, int number, String address, String city, String state, int zip){
-		
-	}
-	public void addService(Date serviceDate, String providerName, String serviceName) {
-		
-	}
-	public void toFile(){
-		
-	}
+
 }
--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ReportController.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ReportController.java
@@ -2,10 +2,5 @@ package chocAnSystem;
 import chocAnSystem.Report;
 
 public class ReportController {
-	public void reportController(model, view) {
-		
-	}
-	public void updateView() {
-		
-	}
+
 }
