aa78215e4d640055e3353be9ce5e0e8a15239195
wwmink <wwmink@crimson.ua.edu>
2023-11-28 14:26:23 -0600

    TBH I forgot everything I did here

--- gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ProviderController.java
+++ gitRepositoryTeam6/chocAnSystem/src/chocAnSystem/ProviderController.java
@@ -12,16 +12,16 @@ public class ProviderController {
     List of Methods we need to implement:
     - verifyProvider
     - verifyMember
-    - addMember (Testing only)
+    - addMember (Testing only) STARTED
         - Needs to save member to file, create new file if it doesn't exist yet
-    - addProvider (Testing only)
+    - addProvider (Testing only) STARTED
         - Needs to save provider to file, create new file if it doesn't exist yet
     - lookupServiceByCode
     - lookupServiceByName
     - listServices
-    - addService (testing only)
+    - addService (testing only) STARTED
         - Needs to save service to file, create new file if it doesn't exist yet
-    - addServiceRecord
+    - addServiceRecord STARTED
         - Needs to save service record to file, create new file if it doesn't exist yet
      */
 
@@ -29,17 +29,58 @@ public class ProviderController {
     Testing method for creating a provider directory with entries. Takes service info and a file path as parameters.
     If the file doesn't exist, it will be created. If it does exist, the new entry will be appended to the end of the file.
      */
-    public void saveServiceType(int code, String name, float fee, String filePath) throws IOException {
+    public void saveServiceType(int code, String name, float fee, String filePath) {
+        // Create new ProviderDirectory object and convert to JSON string
         ProviderDirectory entry = new ProviderDirectory(code, name, fee);
         String jsonString = entry.toJson();
 
+        // Write JSON string to file
         try {
             FileWriter writer = new FileWriter(filePath, true);
             writer.write(jsonString + "\n");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
-            throw new IOException("Error writing to file");
+        }
+    }
+
+    // Method to create / add to file containing provider or member ID Numbers
+    public void saveIDNumber(int idNumber, String filePath) {
+        // Convert ID number to string
+        String idString = Integer.toString(idNumber);
+
+        // Write ID number to file
+        // TODO: Can I make this a separate method? It would make the code more concise
+        // TODO: Make function write to end of array since JSON only allows one object per file
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            writer.write(idString + " ");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to create / add to file containing service records
+    public void saveServiceRecord(String serviceDate, int providerNumber, int memberNumber, int serviceCode, String comments, String filePath) {
+        // Get current date and time
+        Date date = new Date();
+        SimpleDateFormat currentFormatter = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+        String currentDateString = currentFormatter.format(date);
+
+        // Convert service record info to JSON string
+        ServiceRecord record = new ServiceRecord(currentDateString, currentDateString, providerNumber, memberNumber, serviceCode, comments);
+        String jsonString = record.toJson();
+
+        // Write JSON string to file
+        // TODO: Can I make this a separate method? It would make the code more concise
+        // TODO: Make function write to end of array since JSON only allows one object per file
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            writer.write(jsonString + " ");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
